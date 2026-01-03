46729c42725108be6d704eece45c2a6ae54acec6
Ali Abaikhanov <abayhanov1998@gmail.com>
2026-01-03 14:48:44 +0300

    Edit settings.py

--- stripe_server/settings.py
+++ stripe_server/settings.py
@@ -117,9 +117,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Stripe configuration
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY', '')
