e103c9e842c10e2a9cde553d42e073dde172075f
Ali Abaikhanov <abayhanov1998@gmail.com>
2026-01-03 23:49:14 +0300

    Edit settings.py

--- stripe_server/settings.py
+++ stripe_server/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Stripe configuration
